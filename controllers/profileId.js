04375b31033796c26b937a1dea3a9d8fa514e717
Dillon <dillonkmcquade@gmail.com>
2020-03-27 13:36:08 -0400

    working on pw update

--- controllers/profileId.js
+++ controllers/profileId.js
@@ -20,26 +20,43 @@ const handleProfileUpdate = (req, res, db, bcrypt) => {
     return res.status(400).json("incorrect credentials");
   }
   const newHash = bcrypt.hashSync(newPassword);
-
   const currentHash = bcrypt.hashSync(currentPassword);
   const passwordMatch =
     newPassword === confirmPassword
       ? true
       : res.status(400).json("passwords do not match");
-  console.log("updating profile");
-  return db
-    .select("*")
-    .from("users")
-    .where({ id })
-    .then(user => {
-      const isValid = bcrypt.compareSync(currentHash, user[0].hash);
-      if (isValid && passwordMatch) {
-        return res.json(db);
-      } else {
-        return res.status(400).json("wrong credentials");
-      }
-    })
-    .catch(err => res.status(400).json("error updating password"));
+
+  console.log(email);
+
+  if (passwordMatch) {
+    db.select("hash")
+      .from("login")
+      .where("hash", "=", currentHash)
+      .then(data => {
+        const isValid = bcrypt.compareSync(currentHash, data[0].hash);
+        if (isValid) {
+          db("login")
+            .where({ id: id })
+            .update({ hash: newHash })
+            
+        } else {
+          return res.status(400).json("wrong credentials");
+        }
+      })
+      .catch(err => res.status(400).json("Wrong credentials."));
+  }
+  //db.select("*")
+  //.from("users")
+  //.where({ id })
+  //.then(user => {
+  // const isValid = bcrypt.compareSync(currentHash, user[0].hash);
+  // if (isValid && passwordMatch) {
+  //        console.log("valid");
+  //    } else {
+  //    return res.status(400).json("wrong credentials");
+  // }
+  ///   })
+  // .catch(err => res.status(400).json("error updating password"));
 };
 
 module.exports = {
